34827d8278740471eb4a17c242a5142348f9a970
grupokoch <ti@grupokochsa.com.br>
2021-09-15 16:42:59 -0300

    test update archives

--- backend/routes/controller/login/index.js
+++ backend/routes/controller/login/index.js
@@ -6,5 +6,6 @@ module.exports = {
     const response = await db('users').select('*').where({ user, password })
     if (!response.lenght) return res.status(401).json({ auth: false })
     return res.status(200).json({ client: response })
+    /* esse é um teste */
   }
 }
